a7f3f9d6151ed04962c1b6f95712df3052011f24
Dmitrii Frolov <tryfrod@gmail.com>
2025-04-06 20:50:34 +0300

    Простой шаблон, когда есть pos у моба

--- src/main/java/ParserNPC.java
+++ src/main/java/ParserNPC.java
@@ -1,5 +1,5 @@
 import java.io.*;
-import java.nio.charset.Charset;
+//import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
