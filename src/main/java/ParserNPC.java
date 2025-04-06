e10017bd8a3f58722daa4621cd1d3e730bf9ea2a
Dmitrii Frolov <tryfrod@gmail.com>
2025-04-06 20:41:23 +0300

    Простой шаблон, когда есть pos у моба

--- src/main/java/ParserNPC.java
+++ src/main/java/ParserNPC.java
@@ -8,14 +8,14 @@ import java.util.List;
 // respawn_random="0" period_of_day="none"
 
 public class ParserNPC {
-    private final String npcpos_file = "src/main/resources/npcpos.txt";
+     final String npcpos_file = "src/main/resources/npcpos.txt";
     private final Charset charset = Charset.forName("UTF-16LE");
     GetNPC_Id getNPCId = new GetNPC_Id();
 
 
      void parse(){
         getNPCId.getAllNPCID();
-        StringBuffer npcStr = new StringBuffer();
+        StringBuffer npcStringBuffer = new StringBuffer();
         List<String> Npc_Pos = new ArrayList<>();
 
          try {
@@ -26,6 +26,7 @@ public class ParserNPC {
              String[] arrNpc_Pos;
              int respawnTime = 0;
              int total = 0;
+             String npc_Name = null;
              //List<String> territory = new ArrayList<>();
                 while ((str = bufferedReader.readLine()) !=null  ) {
                     if (str.startsWith("territory_begin")) {
@@ -41,6 +42,7 @@ public class ParserNPC {
                         String[] arrNpcmaker_begin = str.split("\t");
                     } else if (str.startsWith("npc_begin")) {
                         String[] arrNpc_begin = str.split("\t");
+                        npc_Name = arrNpc_begin[1].replace("[","").replace("]","").trim();
                         npc_id = getNPCId.getNPC_Id(arrNpc_begin[1]);
 
                         String[] splitPos = arrNpc_begin[2].split("=");
@@ -50,7 +52,7 @@ public class ParserNPC {
                         }
                         else arrNpc_Pos = null;
 
-                        Npc_Pos = Arrays.asList(arrNpc_Pos);
+                        Npc_Pos.addAll(Arrays.asList(arrNpc_Pos));
 
                         String totalStr = (arrNpc_begin[3].replaceAll("[^0-9]",""));
                         total = Integer.parseInt(totalStr);
@@ -62,23 +64,64 @@ public class ParserNPC {
                         else respawnTime = Integer.parseInt(arrNpc_begin[4]);
 
                     }
-                    if (Npc_Pos != null) {
-                        for (int i = 0; i < Npc_Pos.size(); i+=4) {
-                            for (int j = 0; j < 4 ; j++) {
-                                System.out.print("npc_id " + npc_id);
-                                System.out.print(" x = " + Npc_Pos.get(0));
-                                System.out.print(" y = " + Npc_Pos.get(1));
-                                System.out.print(" zmin = " + Npc_Pos.get(2));
-                                System.out.print(" zmax = " + Npc_Pos.get(3));
-                                System.out.println(" total = " + total + " respawnTime = " + respawnTime);
+                    /*
+                    <spawn count="1" respawn="60" respawn_random="0" period_of_day="none">
+		                <territory>
+		                	<add x="47995" y="127717" zmin="-3767" zmax="-2967" />
+		                	<add x="48579" y="129316" zmin="-3767" zmax="-2967" />
+		                	<add x="50391" y="129435" zmin="-3767" zmax="-2967" />
+		                	<add x="49791" y="127264" zmin="-3767" zmax="-2967" />
+		                </territory>
+		              <npc id="20063" /><!--Ol Mahum Shooter-->
+	                </spawn>
+                     */
 
-                            }
-                        }
-
-                    }
                     if (str.startsWith("npcmaker_end")){break;}
                 }
 
+             if (Npc_Pos.size() != 0) {
+                 int cout = 0;
+                 int npcCoord = 0;
+                 for (int i = 0; i < Npc_Pos.size(); i+=4) {
+                     //for (int j = 0; j < 4 ; j++) {
+                                npcStringBuffer.append("<spawn count=\"").append(total)
+                                        .append("\" respawn=\"")
+                                        .append(respawnTime)
+                                        .append("\" respawn_random=\"0\" period_of_day=\"none\">")
+                                        .append("\n")
+                                        .append("\t\t <territory>").append("\n")
+                                        .append("\t\t\t")
+                                        .append("<add x=\"")
+                                        .append(Npc_Pos.get(npcCoord++))
+                                        .append("\" y=\"")
+                                        .append(Npc_Pos.get(npcCoord++))
+                                        .append("\" zmin=\"")
+                                        .append(Npc_Pos.get(npcCoord++))
+                                        .append("\" zmax=\"")
+                                        .append(Npc_Pos.get(npcCoord++))
+                                        .append("\" />")
+                                        .append("\n").append("\t\t")
+                                        .append("</territory>").append("\n")
+                                        .append("\t")
+                                        .append("<npc id=\"")
+                                        .append(npc_id).append("\" /><!--")
+                                        .append(npc_Name).append("-->").append("\n").append("</spawn>").append("\n\n");
+//                     cout++;
+
+//                     System.out.println(cout);
+//
+//                     System.out.print("npc_id " + npc_id);
+//                     System.out.print(" x = " + Npc_Pos.get(npcCoord++));
+//                     System.out.print(" y = " + Npc_Pos.get(npcCoord++));
+//                     System.out.print(" zmin = " + Npc_Pos.get(npcCoord++));
+//                     System.out.print(" zmax = " + Npc_Pos.get(npcCoord++));
+//                     System.out.println(" total = " + total + " respawnTime = " + respawnTime);
+                     //}
+                 }
+                 System.out.print(npcStringBuffer);
+
+
+             }
                 bufferedReader.close();
          } catch (FileNotFoundException e) {
              System.out.println("npcpos.txt not found");
